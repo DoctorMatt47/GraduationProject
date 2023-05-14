896646e77ae14a0749d141b102410ab8aad01918
m.holtsev <DoctorMatt47@gmail.com>
2023-05-15 00:11:10 +0300

    files controller

---
+++ src/WebApi/Controllers/FilesController.cs
@@ -0,0 +1,20 @@
+﻿using GraduationProject.Application.Files;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraduationProject.WebApi.Controllers;
+
+public class FilesController : ApiControllerBase
+{
+    private readonly IFileService _fileService;
+
+    public FilesController(IFileService fileService)
+    {
+        _fileService = fileService;
+    }
+
+    [HttpPost]
+    public async Task UploadFile(CancellationToken cancellationToken)
+    {
+        await _fileService.UploadFile(null, cancellationToken);
+    }
+}

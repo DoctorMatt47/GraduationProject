54a5a361f7e4c55041e5325cc271dca3727b8a7a
m.holtsev <DoctorMatt47@gmail.com>
2023-05-15 00:08:05 +0300

    file record entity

---
+++ src/Domain/Entities/FileRecord.cs
@@ -0,0 +1,24 @@
+﻿namespace GraduationProject.Domain.Entities;
+
+public class FileRecord
+{
+    private FileRecord()
+    {
+    }
+    
+    public string Name { get; private set; } = null!;
+    public string Path { get; private set; } = null!;
+    public string Extension { get; private set; } = null!;
+    
+    public User User { get; private set; } = null!;
+    public Guid UserId { get; private set; } = Guid.Empty;
+
+    public static FileRecord Create(string name, string path, string extension, Guid userId) =>
+        new()
+        {
+            Name = name,
+            Path = path,
+            Extension = extension,
+            UserId = userId,
+        };
+}

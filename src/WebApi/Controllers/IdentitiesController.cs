f888b9311189b71dc7871d59dfe83e68ef6a52cc
m.holtsev <DoctorMatt47@gmail.com>
2023-05-15 00:09:52 +0300

    identities controller

---
+++ src/WebApi/Controllers/IdentitiesController.cs
@@ -0,0 +1,24 @@
+﻿using GraduationProject.Application.Identities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraduationProject.WebApi.Controllers;
+
+public class IdentitiesController : ApiControllerBase
+{
+    private readonly IIdentityService _identityService;
+
+    public IdentitiesController(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+    
+    [HttpPost("Authenticate")]
+    [AllowAnonymous]
+    public Task<IdentityResponse> Authenticate(
+        CreateIdentityRequest body,
+        CancellationToken cancellationToken)
+    {
+        return _identityService.CreateIdentity(body, cancellationToken);
+    }
+}

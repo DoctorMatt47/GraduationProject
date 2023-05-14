f83de9b3ca8df17e11df71e976b8e2ca044bbec9
m.holtsev <DoctorMatt47@gmail.com>
2023-05-15 00:11:24 +0300

    not implemented file service

---
+++ src/Application/Files/FileService.cs
@@ -0,0 +1,20 @@
+﻿using GraduationProject.Application.Common.Abstractions;
+using GraduationProject.Application.Identities;
+using GraduationProject.Domain.Entities;
+
+namespace GraduationProject.Application.Files;
+
+public class FileService : IFileService
+{
+    // private readonly IFileRepository _files;
+    private readonly IIdentityRepository _identities;
+
+    public FileService(IIdentityRepository identities)
+    {
+        _identities = identities;
+    }
+
+    public async Task UploadFile(UploadFileRequest request, CancellationToken cancellationToken = default)
+    {
+    }
+}

c6623fb1b1700d6a0823872612695988babab907
m.holtsev <DoctorMatt47@gmail.com>
2023-05-15 00:09:45 +0300

    users controller

---
+++ src/WebApi/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using GraduationProject.Application.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraduationProject.WebApi.Controllers;
+
+public class UsersController : ApiControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost]
+    [AllowAnonymous]
+    public async Task<CreateUserResponse> CreateUser(CreateUserRequest request, CancellationToken cancellationToken)
+    {
+        return await _userService.CreateUser(request, cancellationToken);
+    }
+}

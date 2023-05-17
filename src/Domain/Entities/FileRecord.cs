0e2c511e029b34f87700d1ddd7b01b59a11a4143
m.holtsev <DoctorMatt47@gmail.com>
2023-05-17 22:35:49 +0300

    file record update fields

--- src/Domain/Entities/FileRecord.cs
+++ src/Domain/Entities/FileRecord.cs
@@ -1,24 +1,25 @@
-﻿namespace GraduationProject.Domain.Entities;
+﻿using GraduationProject.Domain.Abstractions;
 
-public class FileRecord
+namespace GraduationProject.Domain.Entities;
+
+public class FileRecord : IHasId<Guid>
 {
     private FileRecord()
     {
     }
-    
-    public string Name { get; private set; } = null!;
-    public string Path { get; private set; } = null!;
-    public string Extension { get; private set; } = null!;
-    
-    public User User { get; private set; } = null!;
-    public Guid UserId { get; private set; } = Guid.Empty;
 
-    public static FileRecord Create(string name, string path, string extension, Guid userId) =>
+    public string Path { get; private set; } = null!;
+    public long SizeInBytes { get; private set; } = 0;
+
+    public User User { get; private set; } = null!;
+    public Guid UserId { get; private set; }
+
+    public Guid Id { get; private set; } = Guid.NewGuid();
+
+    public static FileRecord Create(string path, Guid userId) =>
         new()
         {
-            Name = name,
             Path = path,
-            Extension = extension,
             UserId = userId,
         };
 }

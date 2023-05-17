38aff34a0cdb54fb56717fc805c33ee3aaa6c9fe
m.holtsev <DoctorMatt47@gmail.com>
2023-05-17 22:36:13 +0300

    add file record domain user method

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -2,6 +2,8 @@
 
 public class User : IdentityUser
 {
+    private readonly List<FileRecord> _fileRecords = new();
+
     private User() : base(Guid.NewGuid())
     {
     }
@@ -10,7 +12,9 @@ public class User : IdentityUser
     public byte[] PasswordSalt { get; private set; } = null!;
     public byte[] PasswordHash { get; private set; } = null!;
     public long MaxBytesAvailable { get; private set; } = 104_857_600;
-    public long BytesUsed { get; private set; } = 0;
+    public long BytesUsed { get; private set; }
+
+    public IEnumerable<FileRecord> FileRecords => _fileRecords.ToList();
 
     public static User Create(string login, byte[] passwordSalt, byte[] passwordHash) =>
         new()
@@ -19,4 +23,10 @@ public class User : IdentityUser
             PasswordSalt = passwordSalt,
             PasswordHash = passwordHash,
         };
+
+    public void AddFileRecord(FileRecord fileRecord)
+    {
+        _fileRecords.Add(fileRecord);
+        BytesUsed += fileRecord.SizeInBytes;
+    }
 }

76fb02351ceb0cbdcc8a64f31d212b73351f2f1d
m.holtsev <DoctorMatt47@gmail.com>
2023-05-21 00:42:10 +0300

    common components

---
+++ src/ClientApp/src/app/common/components/app-button.component.ts
@@ -0,0 +1,15 @@
+﻿import {Component, EventEmitter, Input, Output} from "@angular/core";
+
+@Component({
+    selector: 'app-button',
+    template: `
+      <button [type]="type" class="btn btn-secondary w-100" (click)="click.emit($event)">
+        <ng-content></ng-content>
+      </button>
+    `,
+    styles: [``],
+})
+export class AppButtonComponent {
+  @Input() type: "button" | "submit" | "reset" = "button";
+  @Output() click = new EventEmitter<MouseEvent>();
+}
---
+++ src/ClientApp/src/app/common/components/app-input.component.ts
@@ -0,0 +1,22 @@
+﻿import {Component, EventEmitter, Input, Output} from "@angular/core";
+
+@Component({
+  selector: 'app-input',
+  template: `
+    <div class="form-group">
+      <div class="row justify-content-center">
+        <div class="col-10 col-md-6 col-xl-4">
+          <label class="mt-3 mb-1 w-100">
+            <ng-content></ng-content>
+            <input type="text" [placeholder]="placeholder" class="form-control"
+                   (change)="change.emit($event)" required>
+          </label>
+        </div>
+      </div>
+    </div>
+  `,
+})
+export class AppInputComponent {
+  @Input() placeholder: string = "";
+  @Output() change = new EventEmitter<Event>();
+}

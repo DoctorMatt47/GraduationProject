6394abe981242b7c1738225dda62ccbf7802945e
m.holtsev <DoctorMatt47@gmail.com>
2023-05-21 00:42:26 +0300

    app component and module

--- src/ClientApp/src/app/app.module.ts
+++ src/ClientApp/src/app/app.module.ts
@@ -4,18 +4,26 @@ import {BrowserModule} from "@angular/platform-browser";
 import {AppRoutingModule} from "./app-routing.module";
 import {AppComponent} from "./app.component";
 import {LoginComponent} from "./login/login.component";
+import {AppButtonComponent} from "./common/components/app-button.component";
+import {SignUpComponent} from "./sign-up/sign-up.component";
+import {AppInputComponent} from "./common/components/app-input.component";
+import {SubmitAppButtonComponent} from "./common/components/submit-app-button.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    SignUpComponent,
+    AppButtonComponent,
+    AppInputComponent,
+    SubmitAppButtonComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }

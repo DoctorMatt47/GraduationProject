e168a2b6204b29e7a6b8440258d9cbd3efc3ea48
m.holtsev <DoctorMatt47@gmail.com>
2023-05-23 02:49:12 +0300

    file component update

--- src/ClientApp/src/app/files/files.component.ts
+++ src/ClientApp/src/app/files/files.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import {FileRecord, FilesService} from "./files.service";
+import {Component} from '@angular/core';
+import {FileRecord, FileService} from "./file.service";
+import {firstValueFrom} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-files',
@@ -8,8 +10,43 @@ import {FileRecord, FilesService} from "./files.service";
 })
 export class FilesComponent {
   files: FileRecord[];
+  path: string;
+  bytesAvailable: number = 1000000000;
 
-  constructor(private filesService: FilesService) {
-    this.files = this.filesService.getFilesByDirectory("");
+  constructor(private filesService: FileService, private router: Router) {
+    this.path = this.router.url;
+    this.files = this.filesService.getFilesByDirectory(this.path);
+    console.log(router.getCurrentNavigation());
+  }
+
+  async newFolder() {
+    const folderName = prompt("Enter folder name");
+    if (!folderName) return;
+
+    const fileResponse = await firstValueFrom(this.filesService.uploadFolder(folderName))
+    this.files.push(fileResponse);
+  }
+
+  sizeHumanized(file: FileRecord): string {
+    if (file.isDirectory) return "DIR";
+    if (file.sizeInBytes < 1024) return file.sizeInBytes + " B";
+    if (file.sizeInBytes < 1024 * 1024) return Math.round(file.sizeInBytes / 1024) + " KB";
+    return Math.round(file.sizeInBytes / 1024 / 1024) + " MB";
+  }
+
+  async navigateOrDownload(file: FileRecord) {
+    if (file.isDirectory) {
+      const path = `${this.path}/${file.name}`;
+      await this.router.navigate([path]);
+      this.path = path
+      return;
+    }
+
+    alert("Downloading " + file.name);
+  }
+
+  async uploadFile(event: any) {
+    const fileResponse = await firstValueFrom(this.filesService.uploadFile(event.target.files[0]));
+    this.files.push(fileResponse);
   }
 }

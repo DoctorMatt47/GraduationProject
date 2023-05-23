7074ed96cc3445ae8347344744af68beffb294ed
m.holtsev <DoctorMatt47@gmail.com>
2023-05-23 19:11:25 +0300

    identity service

---
+++ src/ClientApp/src/app/common/data/auth.data.ts
@@ -0,0 +1,38 @@
+﻿import {Injectable} from "@angular/core";
+
+@Injectable()
+export class AuthData {
+  token?: string;
+  id?: string;
+  maxBytesAvailable?: number;
+  bytesUsed?: number;
+
+  constructor() {
+    this.token = localStorage.getItem("token") ?? undefined;
+    this.id = localStorage.getItem("id") ?? undefined;
+
+    const maxBytesAvailable = localStorage.getItem("maxBytesAvailable");
+    this.maxBytesAvailable = maxBytesAvailable === null ?
+      undefined :
+      Number.parseInt(maxBytesAvailable);
+
+    const bytesUsed = localStorage.getItem("bytesUsed");
+    this.bytesUsed = bytesUsed === null ? undefined : Number.parseInt(bytesUsed);
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.token && !!this.id;
+  }
+
+  setData(token: string, id: string, maxBytesAvailable: number, bytesUsed: number) {
+    this.token = token;
+    this.id = id;
+    this.maxBytesAvailable = maxBytesAvailable;
+    this.bytesUsed = bytesUsed;
+
+    localStorage.setItem("token", token);
+    localStorage.setItem("id", id);
+    localStorage.setItem("maxBytesAvailable", maxBytesAvailable.toString());
+    localStorage.setItem("bytesUsed", bytesUsed.toString());
+  }
+}
---
+++ src/ClientApp/src/app/common/services/identity.service.ts
@@ -0,0 +1,55 @@
+﻿import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../enviroment";
+import {firstValueFrom, Observable} from "rxjs";
+import {AuthData} from "../data/auth.data";
+
+interface IdentityRequest {
+  login: string;
+  password: string;
+}
+
+interface IdentityResponse {
+  token: string;
+  id: string;
+  maxBytesAvailable: number;
+  bytesUsed: number;
+}
+
+@Injectable()
+export class IdentityService {
+  identitiesUrl = environment.baseUrl + "identities/";
+  usersUrl = environment.baseUrl + "users/";
+
+
+  constructor(private http: HttpClient, private authData: AuthData) {
+
+  }
+
+  async login({login, password} : IdentityRequest): Promise<IdentityResponse> {
+    const response = await firstValueFrom(
+        this.http.post<IdentityResponse>(this.identitiesUrl + "authenticate", {login, password}));
+
+    this.authData.setData(
+      response.token,
+      response.id,
+      response.maxBytesAvailable,
+      response.bytesUsed);
+
+    return response;
+  }
+
+  async signUp(request: IdentityRequest): Promise<IdentityResponse> {
+    const response =
+      await firstValueFrom(
+      this.http.post<IdentityResponse>(this.usersUrl, request))
+
+    this.authData.setData(
+      response.token,
+      response.id,
+      response.maxBytesAvailable,
+      response.bytesUsed);
+
+    return response;
+  }
+}

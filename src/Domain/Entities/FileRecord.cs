fa01a55db9c69d6f145d8773e285f3838c9f1680
m.holtsev <DoctorMatt47@gmail.com>
2023-05-24 02:39:24 +0300

    file record upload at

--- src/Domain/Entities/FileRecord.cs
+++ src/Domain/Entities/FileRecord.cs
@@ -10,15 +10,23 @@ public class FileRecord : IHasId<Guid>
 
     public string Path { get; private set; } = null!;
     public long SizeInBytes { get; private set; }
+    public DateTimeOffset UploadedAt { get; private set; } = DateTimeOffset.UtcNow;
 
     public User User { get; private set; } = null!;
     public Guid UserId { get; private set; } = Guid.Empty;
 
     public Guid Id { get; private set; } = Guid.NewGuid();
-
-    public static FileRecord Create(string path) =>
+    
+    public static FileRecord Create(string path, long sizeInBytes) =>
         new()
         {
             Path = path,
+            SizeInBytes = sizeInBytes,
         };
+
+    public void UpdateSize(long sizeInBytes)
+    {
+        SizeInBytes = sizeInBytes;
+        UploadedAt = DateTimeOffset.UtcNow;
+    }
 }

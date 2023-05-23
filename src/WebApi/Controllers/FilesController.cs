506f7d7841eaa60e46cc1fce8cc67e45244743e9
m.holtsev <DoctorMatt47@gmail.com>
2023-05-24 02:39:46 +0300

    download file and get files endpoints

--- src/WebApi/Controllers/FilesController.cs
+++ src/WebApi/Controllers/FilesController.cs
@@ -1,4 +1,6 @@
-﻿using GraduationProject.Application.Files;
+﻿using System.Net;
+using System.Net.Http.Headers;
+using GraduationProject.Application.Files;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GraduationProject.WebApi.Controllers;
@@ -9,13 +11,33 @@ public class FilesController : ApiControllerBase
 
     public FilesController(IFileService fileService) => _fileService = fileService;
 
-    [HttpPost]
-    public async Task UploadFile(IFormFile file, CancellationToken cancellationToken)
+    [HttpPost("{path}")]
+    public async Task UploadFile(string path, IFormFile file, CancellationToken cancellationToken)
     {
         using var stream = new MemoryStream();
         await file.CopyToAsync(stream, cancellationToken);
 
-        var request = new UploadFileRequest(stream, file.Length, file.Name);
+        var request = new UploadFileRequest(
+            stream,
+            Path.Combine(path, file.FileName)
+                .Replace("\\", "/")
+                .Replace("%2F", "/"));
+        
         await _fileService.UploadFile(request, cancellationToken);
+    }
+    
+    [HttpGet("{path}")]
+    public async Task<IActionResult> DownloadFile(string path, CancellationToken cancellationToken)
+    {
+        var filteredPath = path.Replace("%2F", "/");
+        var fileStream = await _fileService.DownloadFile(filteredPath, cancellationToken);
+
+        return File(fileStream, "application/octet-stream", filteredPath.Split("/").Last());
+    }
+    
+    [HttpGet]
+    public async Task<IEnumerable<FileRecordResponse>> GetFileRecords(CancellationToken cancellationToken)
+    {
+        return await _fileService.GetFileRecords(cancellationToken);
     }
 }
